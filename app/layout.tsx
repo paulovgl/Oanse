d5ce2b1c2418581d255cf3d1649da5ed5f55d2c3
PauloVGL <ditoprimo@gmail.com>
2024-03-07 22:26:09 -0300

    initial project finish

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,16 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Oanse Dashboard',
+    default: 'Oanse Dashboard'
+  },
+  description: 'O site criado para auxiliar os diretores Oanse.',
+  metadataBase: new URL('https://www.instagram.com/oansebrasil/'),
+};
+
 export default function RootLayout({
   children,
 }: {
